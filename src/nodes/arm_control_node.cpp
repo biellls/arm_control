8a3dad856f5663d29b4b90e648a0968e0c290dbe
Stephan Wirth <stephan.wirth@uib.es>
2011-08-01 14:17:54 +0200

    some refactoring to work without actions, arm_control_node is now lower-level

--- src/nodes/arm_control_node.cpp
+++ src/nodes/arm_control_node.cpp
@@ -1,10 +1,8 @@
 #include <queue>
 
 #include <ros/ros.h>
-
+#include <std_msgs/Bool.h>
 #include <tf/transform_broadcaster.h>
-#include <actionlib/server/simple_action_server.h>
-#include "arm_control/MoveArmAction.h"
 
 #include "melfa_ros/conversions.h"
 
@@ -12,24 +10,21 @@
 #include "melfa/robot_pose.h"
 #include "melfa/exceptions.h"
 
-namespace ac = arm_control;
-
 class ArmControlNode
 {
     ros::NodeHandle nh_;
     ros::NodeHandle nh_private_;
 
     ros::Publisher pose_pub_;
+    ros::Publisher busy_status_pub_;
+    ros::Subscriber target_pose_sub_;
+
     tf::TransformBroadcaster tf_broadcaster_;
     melfa::Melfa melfa_;
     ros::Timer timer_;
 
-    actionlib::SimpleActionServer<ac::MoveArmAction> action_server_;
-
-    std::queue<melfa::RobotPose> way_points_;
-
   public:
-    ArmControlNode() : nh_private_("~"), action_server_(nh_, "arm_control_action_server", false)
+    ArmControlNode() : nh_("robot_arm"), nh_private_("~")
     {
         init();
     }
@@ -40,11 +35,20 @@ class ArmControlNode
 
     void init()
     {
-        pose_pub_ = nh_.advertise<geometry_msgs::PoseStamped>("pose", 1);
-
         std::string device;
         nh_private_.param<std::string>("device", device, "/dev/ttyUSB0");
         ROS_INFO("using device %s", device.c_str());
+        double acceleration;
+        nh_private_.param<double>("acceleration", acceleration, 100);
+        double maximum_velocity;
+        nh_private_.param<double>("maximum_velocity", maximum_velocity, 0.05);
+        melfa::RobotPose tool_pose;
+        nh_private_.param<double>("tool_pose_x", tool_pose.x, 0.0);
+        nh_private_.param<double>("tool_pose_y", tool_pose.x, 0.0);
+        nh_private_.param<double>("tool_pose_z", tool_pose.x, 0.0);
+        nh_private_.param<double>("tool_pose_roll", tool_pose.roll, 0.0);
+        nh_private_.param<double>("tool_pose_pitch", tool_pose.pitch, 0.0);
+        nh_private_.param<double>("tool_pose_yaw", tool_pose.yaw, 0.0);
 
         try
         {
@@ -52,6 +56,13 @@ class ArmControlNode
             params.device = device;
             melfa_.setParams(params);
             melfa_.connect();
+            ROS_INFO("Robot connected.");
+            melfa_.setAcceleration(acceleration);
+            ROS_INFO("Acceleration set to %f", acceleration);
+            melfa_.setMaximumVelocity(maximum_velocity);
+            ROS_INFO("Maximum velocity set to %f", maximum_velocity);
+            melfa_.setToolPose(tool_pose);
+            ROS_INFO_STREAM("Tool pose set to " << tool_pose);
         }
         catch (melfa::MelfaSerialConnectionError& err)
         {
@@ -62,102 +73,18 @@ class ArmControlNode
             ROS_ERROR("Robot error: %s", err.what());
         }
 
-        timer_ = nh_.createTimer(ros::Duration(0.05), boost::bind(&ArmControlNode::reportPose, this)); 
-        action_server_.registerGoalCallback(boost::bind(&ArmControlNode::goalCB, this));
-        action_server_.start();
+        pose_pub_ = nh_.advertise<geometry_msgs::PoseStamped>("pose", 1);
+        busy_status_pub_ = nh_.advertise<std_msgs::Bool>("busy_status", 1);
+
+        target_pose_sub_ = nh_.subscribe("target_pose", 1, &ArmControlNode::setTargetPose, this);
+
+        timer_ = nh_.createTimer(ros::Duration(0.05), boost::bind(&ArmControlNode::report, this)); 
     }
 
-    void goalCB()
+    void report()
     {
-        ac::MoveArmGoalConstPtr goal = action_server_.acceptNewGoal();
-        way_points_ = readWayPoints(goal->path);
-        try
-        {
-            // set parameters
-            melfa_.setMaximumVelocity(goal->maximum_velocity);
-            melfa_.setAcceleration(goal->acceleration);
-            melfa::RobotPose tool_pose;
-            melfa_ros::poseMsgToRobot(goal->tool_pose, tool_pose);
-            melfa_.setToolPose(tool_pose);
-            while (way_points_.size() > 0 && ros::ok())
-            {
-                try
-                {
-                    // get current pose as feedback
-                    ac::MoveArmFeedback feedback;
-                    tf::poseTFToMsg(retrievePose(), feedback.current_pose);
-                    action_server_.publishFeedback(feedback);
-
-                    if (action_server_.isPreemptRequested())
-                    {
-                        ROS_INFO("MoveArmAction preempted.");
-                        action_server_.setPreempted();
-                        melfa_.stop();
-                        break;
-                    }
-                    melfa::RobotPose& next_target_pose = way_points_.front();
-                    melfa_.moveTo(next_target_pose);
-                    way_points_.pop();
-                    ROS_INFO("Sent next way point to robot: %f %f %f, %f %f %f",
-                            next_target_pose.x,
-                            next_target_pose.y,
-                            next_target_pose.z,
-                            next_target_pose.roll,
-                            next_target_pose.pitch,
-                            next_target_pose.yaw);
-                }
-                catch (const melfa::MelfaRobotBusyException&)
-                {
-                    // move command failed because robot is busy, wait a little
-                    ROS_INFO("Robot is busy, waiting 1 second to send next waypoint.");
-                    sleep(1);
-                }
-            }
-        }
-        catch (const melfa::MelfaException& e)
-        {
-            ROS_ERROR("Exception occured when moving robot: %s", e.what());
-            action_server_.setAborted();
-        }
-        // motion is finished
-        ac::MoveArmResult result;
-        tf::poseTFToMsg(retrievePose(), result.end_pose);
-        action_server_.setSucceeded(result);
-    }
-
-    std::queue<melfa::RobotPose> readWayPoints(const geometry_msgs::PoseArray& pose_array) const
-    {
-        std::queue<melfa::RobotPose> way_points;
-        for (size_t i = 0; i < pose_array.poses.size(); ++i)
-        {
-            const geometry_msgs::Pose& pose = pose_array.poses[i];
-            melfa::RobotPose way_point;
-            melfa_ros::poseMsgToRobot(pose, way_point);
-            way_points.push(way_point);
-        }
-        return way_points;
-    }
-
-    /// reads the pose from the robot and fills the given tf struct
-    tf::Pose retrievePose()
-    {
-        melfa::RobotPose robot_pose = melfa_.getPose();
-        tf::Quaternion quat;
-        quat.setRPY(robot_pose.roll, robot_pose.pitch, robot_pose.yaw);
-        tf::Pose tf_pose(quat, tf::Vector3(robot_pose.x, robot_pose.y, robot_pose.z));
-        ROS_DEBUG("Retrieved pose: %f %f %f, %f %f %f", 
-                robot_pose.x, 
-                robot_pose.y, 
-                robot_pose.z, 
-                robot_pose.roll, 
-                robot_pose.pitch, 
-                robot_pose.yaw);
-        return tf_pose;
-    }
-
-
-    void reportPose()
-    {
+        std_msgs::Bool busy_msg;
+        busy_msg.data = false;
         try
         {
             geometry_msgs::PoseStamped pose_stamped;
@@ -173,13 +100,43 @@ class ArmControlNode
                     tf::StampedTransform(
                         tf::Transform(tf_pose),
                         timestamp, "arm_base", "arm_tool"));
+
+            // try some execution command
+            // and check if it throws to see if the robot is busy
+            melfa_.execute("P1=P_CURR");
+        }
+        catch (const melfa::MelfaRobotBusyException&)
+        {
+            // robot is busy
+            busy_msg.data = true;
         }
         catch (const melfa::MelfaException& e)
         {
             ROS_ERROR("Exception occured when trying to retrieve robot pose: %s", e.what());
-            action_server_.setAborted();
         }
-     }
+        busy_status_pub_.publish(busy_msg);
+    }
+
+    void setTargetPose(const geometry_msgs::PoseConstPtr& pose_msg)
+    {
+        ROS_INFO_STREAM("Received new pose request: " << *pose_msg);
+        try
+        {
+            melfa::RobotPose robot_pose;
+            melfa_ros::poseMsgToRobot(*pose_msg, robot_pose);
+            melfa_.moveTo(robot_pose);
+        }
+        catch (const melfa::MelfaRobotBusyException&)
+        {
+            // robot is busy
+            ROS_ERROR("Cannot fulfill pose request, robot is busy!");
+        }
+        catch (const melfa::MelfaException& e)
+        {
+            ROS_ERROR("Exception occured when trying to move robot: %s", e.what());
+        }
+    }
+
 };
 
 int main(int argc, char** argv)
--- src/nodes/follow_path.cpp
+++ src/nodes/follow_path.cpp
@@ -8,23 +8,7 @@
 #include "arm_control/MoveArmAction.h"
 
 #include "melfa/robot_pose.h"
-
-void operator >> (const YAML::Node& node, melfa::RobotPose& pose)
-{
-    node[0] >> pose.x;
-    node[1] >> pose.y;
-    node[2] >> pose.z;
-    node[3] >> pose.roll;
-    node[4] >> pose.pitch;
-    node[5] >> pose.yaw;
-}
-
-std::ostream& operator<< (std::ostream& ostr, melfa::RobotPose& pose)
-{
-    ostr << "(" << pose.x << ", " << pose.y << ", " << pose.z << ") "
-         << "(" << pose.roll << ", " << pose.pitch << ", " << pose.yaw << ")";
-    return ostr;
-}
+#include "melfa_ros/robot_path.h"
 
 int main (int argc, char **argv)
 {
@@ -35,75 +19,32 @@ int main (int argc, char **argv)
         exit(1);
     }
 
-    std::ifstream in(argv[1]);
-    if (!in.is_open())
+    melfa_ros::RobotPath robot_path = melfa_ros::readRobotPath(argv[1]);
+
+    /*
+    // insert data from RobotPath struct
+    melfa_ros::poseRobotToMsg(robot_path.tool_pose, goal.tool_pose);
+    goal.path.resize(robot_path.path_poses.size());
+    for (size_t i = 0; i < robot_path.path_poses.size(); ++i)
     {
-        std::cerr << "Cannot open file " << argv[1] << std::endl;
-        exit(2);
+        melfa_ros::poseRobotToMsg(robot_path.path_poses[i], goal.path[i]);
     }
 
-    try
+    ac.sendGoal(goal);
+
+    //wait for the action to return
+    bool finished_before_timeout = ac.waitForResult(ros::Duration(300.0));
+
+    if (finished_before_timeout)
     {
-        YAML::Parser parser(in);
-        if (!parser)
-        {
-            std::cerr << "Cannot create YAML parser." << std::endl;
-            exit(3);
-        }
-
-        YAML::Node doc;
-        parser.GetNextDocument(doc);
-
-        double acceleration;
-        double maximum_velocity;
-        doc["acceleration"] >> acceleration;
-        doc["maximum_velocity"] >> maximum_velocity;
-        std::cout << "acceleration = " << acceleration << std::endl;
-        std::cout << "maximum_velocity = " << maximum_velocity << std::endl;
-        melfa::RobotPose tool_pose;
-        doc["tool_pose"] >> tool_pose;
-        std::cout << "tool_pose = " << tool_pose << std::endl;
-        int num_poses = doc["path"].size();
-        std::vector<melfa::RobotPose> robot_poses(num_poses);
-        std::cout << "Reading " << robot_poses.size() << " way points." << std::endl;
-        for (size_t i = 0; i < doc["path"].size(); ++i)
-        {
-            doc["path"][i] >> robot_poses[i];
-            std::cout << i << "\t" << robot_poses[i] << std::endl;
-        }
-        // create the action client
-        // true causes the client to spin its own thread
-        actionlib::SimpleActionClient<arm_control::MoveArmAction> ac("follow_path_action_client", true);
-
-        ROS_INFO("Waiting for action server to start.");
-        // wait for the action server to start
-        ac.waitForServer(); //will wait for infinite time
-
-        ROS_INFO("Action server started, sending goal.");
-        // send a goal to the action
-        arm_control::MoveArmGoal goal;
-        goal.acceleration = acceleration;
-        goal.maximum_velocity = maximum_velocity;
-
-        ac.sendGoal(goal);
-
-        //wait for the action to return
-        bool finished_before_timeout = ac.waitForResult(ros::Duration(300.0));
-
-        if (finished_before_timeout)
-        {
-            actionlib::SimpleClientGoalState state = ac.getState();
-            ROS_INFO("Action finished: %s", state.toString().c_str());
-        }
-        else
-            ROS_INFO("Action did not finish before the time out.");
-
+        actionlib::SimpleClientGoalState state = ac.getState();
+        ROS_INFO("Action finished: %s", state.toString().c_str());
     }
-    catch (const YAML::Exception& e)
+    else
     {
-        std::cerr << "YAML exception: " << e.what() << std::endl;
-        exit(4);
+        ROS_INFO("Action did not finish before the time out.");
     }
+    */
 
     return 0;
 }
